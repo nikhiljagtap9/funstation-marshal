3a01e19b5c6c3923d87d3f6d7185fcfdb47bd4c6
HtooAg <htoow243@gmail.com>
2025-07-14 23:49:52 +0400

    Added cloud

--- types/global.d.ts
+++ types/global.d.ts
@@ -5,8 +5,10 @@ declare global {
 				message?: string;
 				time?: string;
 				teamId?: string;
+				gameIndex?: number;
 				completedGames?: number;
 				resultsRevealed?: boolean; // Add this line for admin settings
+				hasGameProgress?: boolean;
 		  }) => void)
 		| undefined;
 }
